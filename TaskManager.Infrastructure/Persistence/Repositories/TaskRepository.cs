13815a95617cbc1b158ae20bb26100ed717747b6
Sabbir Ahmed Shubo <sabbir.ove@gmail.com>
2026-01-04 16:37:39 +0600

    Add persistance layer

---
+++ TaskManager.Infrastructure/Persistence/Repositories/TaskRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Application.Abstractions.Persistence;
+using TaskManager.Domain.Entities;
+
+namespace TaskManager.Infrastructure.Persistence.Repositories;
+
+public class TaskRepository : ITaskRepository
+{
+    private readonly TaskDbContext _db;
+
+    public TaskRepository(TaskDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task AddAsync(TaskItem task, CancellationToken ct)
+    {
+        _db.Tasks.Add(task);
+        await _db.SaveChangesAsync(ct);
+    }
+
+    public async Task<List<TaskItem>> GetAllAsync(CancellationToken ct)
+    {
+        return await _db.Tasks.AsNoTracking().ToListAsync(ct);
+    }
+}
---
+++ TaskManager.Infrastructure/Persistence/TaskDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using TaskManager.Domain.Entities;
+
+namespace TaskManager.Infrastructure.Persistence;
+
+public class TaskDbContext : DbContext
+{
+    public TaskDbContext(DbContextOptions<TaskDbContext> options)
+        : base(options) { }
+
+    public DbSet<TaskItem> Tasks => Set<TaskItem>();
+
+}
